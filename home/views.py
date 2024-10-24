5bf27eab4dfb7969b9e42281aa6ba02a8065a1de
rohandhiman5 <143100827+rohandhiman5@users.noreply.github.com>
2024-10-25 02:00:45 +0530

    intital commit

---
+++ home/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Create your views here.
+import pickle
+
+
+# Load model and vectorizer once, globally
+model = pickle.load(open("static/ML/NB.pkl", "rb"))
+vectorizer = pickle.load(open("static/ML/Vectorizer_logistic_regression.pkl", "rb"), encoding='latin1')  # Added encoding
+
+
+def index(request):
+    return render(request,'index.html')
+
+
+def getPrediction(mail):
+    
+    input_vector=vectorizer.transform([mail])
+
+    prediction=model.predict(input_vector)
+
+    return prediction[0]
+
+
+
+
+def result(request):
+    input=request.GET.get('email')
+
+    prediction=getPrediction(input)
+
+    return render(request,'result.html',{'output':prediction})
+
+
+
+
+
+
+
+
